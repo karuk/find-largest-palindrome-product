ecf4796f3ad7137e7514a07689c41f398caf4bc1
karuk <karuna_gujar@yahoo.com>
2017-04-27 02:25:54 -0500

    main driver file added

---
+++ src/checkPalindrome/MainFindLargestPalindrome.java
@@ -0,0 +1,11 @@
+package checkPalindrome;
+
+/**
+ * @author  Karuna Gujar
+ */
+public class FindLargestPalindrome {
+    public static void main (String[] args) {
+        int largestPalindrome = CheckPalindrome.findLargestPalindromeProduct();
+        System.out.println(largestPalindrome);
+    }
+}
